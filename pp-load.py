484b91923ed882f7b50909997cfde77de537c343
jwestgard <jwestgard@mac.com>
2016-09-05 22:49:52 -0400

    sparql update work

--- pp-load.py
+++ pp-load.py
@@ -5,10 +5,10 @@ from __future__ import print_function
 import argparse
 from csv import DictReader
 import hashlib
-import os
-import pprint
-import requests
 from io import BytesIO
+import os
+import requests
+import sys
 import yaml
 
 from rdflib import Graph, Literal, BNode, RDF, URIRef
@@ -41,7 +41,7 @@ ns = {
 # verify communication with repository
 def check_connection(endpoint, auth):
     response = requests.get(endpoint, auth=auth)
-    if response == 200:
+    if response.status_code == 200:
         return True
     else:
         return False
@@ -58,7 +58,7 @@ class Resource():
 
     def __init__(self, metadata):
         self.graph = Graph()
-        self.subj = URIRef('')
+        self.uri = URIRef('')
         self.__dict__.update(metadata)
         namespace_manager = NamespaceManager(Graph())
         for k, v in ns.items():
@@ -72,36 +72,79 @@ class Resource():
 
 
     # create an empty resource
-    def create_rdf(*kwargs):
-        response = requests.post(REST_ENDPOINT, auth=auth)
+    def create_rdf(self, endpoint, auth):
+        response = requests.post(endpoint, auth=auth)
         if response.status_code == 201:
-            self.uri = response.text
+            self.uri = REST_ENDPOINT + response.text[len(endpoint):]
             return True
         else:
             return False
 
 
-    def create_nonrdf(*kwargs):
-        data = open(self.file, 'rb').read()
-        headers = {'Content-Type': 'application/octet-stream',
+    # upload a binary resource
+    def create_nonrdf(self, endpoint, auth):
+        data = open(self.local_path, 'rb').read()
+        headers = {'Content-Type': 'application/pdf',
                    'Digest': 'sha1={0}'.format(self.checksum),
                    'Content-Disposition': 
                         'attachment; filename="{0}"'.format(self.filename)
                     }
-        response = requests.post(uri, auth=auth, data=data, headers=headers)
+        response = requests.post(endpoint, 
+                                 auth=auth, 
+                                 data=data, 
+                                 headers=headers
+                                 )
         if response.status_code == 201:
-            self.uri = response.text
+            self.uri = REST_ENDPOINT + response.text[len(endpoint):]
             return True
         else:
             return False
 
 
+    # update the subject of graph after URI assignment
+    def update_graph(self):
+        for (s, p, o) in self.graph:
+            self.graph.remove((s, p, o))
+            self.graph.add((URIRef(self.uri), p, o))
+
+
     # update existing resource with sparql update
-    def sparql_update(transaction):
-        response = requests.patch(self.uri, auth=auth, data=self.graph)
+    def sparql_update(self, endpoint, auth, triples):
+        data = ['PREFIX pcdm: <http://pcdm.org/models#>']
+        data.append('INSERT {')
+        for (prefix, predicate, object) in triples:
+            data.append('<> {0}:{1} "{2}" .'.format(prefix, predicate, object))
+        data.append('} WHERE { }')
+        payload = '\n'.join(data)
+        headers = {'Content-Type': 'application/sparql-update'}
+        response = requests.patch(endpoint, 
+                                  auth=auth, 
+                                  data=payload,
+                                  headers=headers
+                                  )
         if response.status_code == 204:
             return True
         else:
+            print(response, response.headers)
+            print(response.text)
+            return False
+    
+    
+    # replace triples on existing resource
+    def put_graph(self, endpoint, auth):
+        data = self.graph.serialize(format='turtle')
+        headers = {'Content-Type': 'text/turtle'}
+        response = requests.put(endpoint, 
+                                auth=auth, 
+                                data=data,
+                                headers=headers
+                                )
+        if response.status_code == 204:
+            return True
+        else:
+            print(response)
+            print(response.headers)
+            print(response.text)
             return False
 
 
@@ -116,54 +159,54 @@ class Patent(Resource):
         self.filename = os.path.basename(self.asset_path)
         self.file_metadata = {'pages': self.pages, 
                               'scan_date': self.scan_date,
-                              'asset_path': self.asset_path
+                              'local_path': self.asset_path
                               }
         self.file = File(self.file_metadata)
         
         self.graph.add(
-            (self.subj, ns['dc'].title, Literal(self.title))
+            (self.uri, ns['dc'].title, Literal(self.title))
             )
         self.graph.add(
-            (self.subj, ns['uspatent-s'].docNo, Literal(self.patent_number))
+            (self.uri, ns['uspatent-s'].docNo, Literal(self.patent_number))
             )
         self.graph.add(
-            (self.subj, ns['dc'].date, Literal(self.date,
+            (self.uri, ns['dc'].date, Literal(self.date,
                                                 datatype=ns['xsd'].date))
             )
         self.graph.add(
-            (self.subj, ns['dc'].subject, Literal(self.large_category))
+            (self.uri, ns['dc'].subject, Literal(self.large_category))
             )
         self.graph.add(
-            (self.subj, ns['bibo'].webpage, URIRef(self.patent_url))
+            (self.uri, ns['bibo'].webpage, URIRef(self.patent_url))
             )
         self.graph.add(
-            (self.subj, ns['rdf'].type, ns['pcdm'].Object)
+            (self.uri, ns['rdf'].type, ns['pcdm'].Object)
             )
         self.graph.add(
-            (self.subj, ns['ex'].otherid, Literal(self.uspc))
+            (self.uri, ns['ex'].otherid, Literal(self.uspc))
             )
         self.graph.add(
-            (self.subj, ns['ex'].otherid, Literal(self.application_number))
+            (self.uri, ns['ex'].otherid, Literal(self.application_number))
             )
         for inventor in self.inventor.split(';'):
             if inventor is not "":
                 self.graph.add(
-                    (self.subj, ns['dc'].creator, Literal(inventor))
+                    (self.uri, ns['dc'].creator, Literal(inventor))
                     )
         for city in self.city.split(';'):
             if city is not "":
                 self.graph.add(
-                    (self.subj, ns['ex'].inventorCity, Literal(city))
+                    (self.uri, ns['ex'].inventorCity, Literal(city))
                     )
         for state in self.state.split(';'):
             if state is not "":
                 self.graph.add(
-                    (self.subj, ns['ex'].inventorState, Literal(state))
+                    (self.uri, ns['ex'].inventorState, Literal(state))
                     )
         for country in self.country.split(';'):
             if country is not "":
                 self.graph.add(
-                    (self.subj, ns['ex'].inventorCountry, Literal(country))
+                    (self.uri, ns['ex'].inventorCountry, Literal(country))
                     )
 
 
@@ -174,38 +217,38 @@ class File(Resource):
     
     def __init__(self, metadata):
         Resource.__init__(self, metadata)
-        self.filename = os.path.basename(self.asset_path)
-        self.checksum = self.sha1(self.asset_path)
+        self.filename = os.path.basename(self.local_path)
+        self.checksum = self.sha1()
         
         self.graph.add(
-            (self.subj, ns['rdf'].type, ns['pcdm'].File)
+            (self.uri, ns['rdf'].type, ns['pcdm'].File)
             )
         self.graph.add(
-            (self.subj, ns['bibo'].numPages, Literal(self.pages,
+            (self.uri, ns['bibo'].numPages, Literal(self.pages,
                                                   datatype=ns['xsd'].integer))
             )
         self.graph.add(
-            (self.subj, ns['dc'].date, Literal(self.scan_date, 
+            (self.uri, ns['dc'].date, Literal(self.scan_date, 
                                                datatype=ns['xsd'].datetime))
             )
         self.graph.add(
-            (self.subj, ns['rdf'].type, ns['pcdm'].File)
+            (self.uri, ns['rdf'].type, ns['pcdm'].File)
             )
     
     
     # confirm accessibility of a local asset
     def file_exists(self):
-        if os.path.isfile(self.filepath):
+        if os.path.isfile(self.local_path):
             return True
         else:
             return False
     
     
     # generate SHA1 checksum on a file
-    def sha1(self, path):
+    def sha1(self):
         BUF_SIZE = 65536
         sha1 = hashlib.sha1()
-        with open(path, 'rb') as f:
+        with open(self.local_path, 'rb') as f:
             while True:
                 data = f.read(BUF_SIZE)
                 if not data:
@@ -217,32 +260,29 @@ class File(Resource):
 
 class Transaction():
 
-    ''' wrapper class for managing a Fedora 4 transaction 
+    ''' Wrapper class for managing a Fedora 4 transaction 
 
         TRANSACTION SYNTAX:
         ===================
         start:      /fcr:tx
         act:        /tx:{transaction_id}/path/to/resource
         keepalive:  /tx:{transaction_id}/fcr:tx
-        commit      /tx:{transaction_id}/fcr:commit
-        rollback    /tx:{transaction_id}/fcr:rollback '''
+        commit      /tx:{transaction_id}/fcr:tx/fcr:commit
+        rollback    /tx:{transaction_id}/fcr:tx/fcr:rollback '''
 
     def __init__(self, endpoint, auth):
         response = requests.post('{0}/fcr:tx'.format(endpoint), auth=auth)
         if response.status_code == 201:
             self.auth = auth
-            self.id = response.headers['Location']
-            self.uri = "{0}/tx:{1}".format(endpoint, self.id)
-            self.commit_uri = "{0}/fcr:commit".format(self.uri)
-            self.rollback_uri = "{0}/fcr:rollback".format(self.uri)
-            return self.uri
-        else:
-            return False
+            self.uri = response.headers['Location']
+            self.id = self.uri[len(endpoint):]
+            self.commit_uri = "{0}/fcr:tx/fcr:commit".format(self.uri)
+            self.rollback_uri = "{0}/fcr:tx/fcr:rollback".format(self.uri)
 
 
     # commit transaction
-    def transaction_commit():
-        response = requests.post(self.commit_uri, auth=auth)
+    def commit(self):
+        response = requests.post(self.commit_uri, auth=self.auth)
         if response.status_code == 204:
             return True
         else:
@@ -250,8 +290,8 @@ class Transaction():
 
 
     # rollback transaction
-    def transaction_rollback(transaction_id, auth):
-        response = requests.post(self.rollback_uri, auth=auth)
+    def rollback(self):
+        response = requests.post(self.rollback_uri, auth=self.auth)
         if response.status_code == 204:
             return True
         else:
@@ -291,7 +331,7 @@ def main():
 
     # open a file to log assets loaded and URIs
     print('Opening log file: {0}'.format(LOG_FILE))
-    logfile = open(LOG_FILE, 'w')
+    logfile = open(LOG_FILE, 'w', 1)
 
     # load metadata file and parse
     print('Reading metadata file: {0}'.format(METADATA_FILE))
@@ -310,81 +350,93 @@ def main():
                                         
     print('  => Found {0} local assets'.format(len(os.listdir(ASSET_DIR))))
     
+    # check repository connection
+    print("Checking repository connection... ", end='')
+    if check_connection(REST_ENDPOINT, auth):
+        print("success!")
+    else:
+        print("failed!")
+        print("Verify that your repository is online and try again.")
+        sys.exit()
+    
+    
     # loop over metadata, generate containers, and POST to fcrepo
     for n, row in enumerate(metadata):
-        p = Patent(row)
-        print("\n{0}. {1}: {2}".format(n+1, p.title, p.filename))
-        print("  => Local path: {0}".format(p.asset_path))
-        print("  => SHA1 checksum: {0}".format(p.file.checksum))
-        
-        # open transaction, create objects, update objects, commit
-        
-        print()
+        patent = Patent(row)
+        print("\n{0}. {1}: {2}".format(n+1, patent.title, patent.filename))
+        print("  => Local path: {0}".format(patent.asset_path))
+        print("  => SHA1 checksum: {0}\n".format(patent.file.checksum))
         
         # open transaction
         print("Opening transaction to create resources... ", end='')
-        print()
+        transaction = Transaction(REST_ENDPOINT, auth)
+        print("ID:{0}".format(transaction.id))
         
         # create empty container
         print("Creating repository container... ", end='')
-        print()
+        if patent.create_rdf(transaction.uri, auth):
+            print("success!")
+        else:
+            print("failed!")
+            transaction.rollback()
+            continue
         
         # create binary
         print("Uploading binary reource... ", end='')
-        print()
+        if patent.file.create_nonrdf(transaction.uri, auth):
+            print("success!")
+        else:
+            print("failed!")
+            transaction.rollback()
+            continue
         
-        # update patent metadata graph
-        print("Updating patent metadata graph... ", end='')
-        print()
-        print()
-        p.print_graph()
+        # update and patch patent metadata graph
+        '''print("Updating patent metadata graph... ", end='')
+        sparql_uri = "{0}{1}{2}".format(REST_ENDPOINT,
+                                        transaction.id,
+                                        patent.uri[len(REST_ENDPOINT):]
+                                        )
+        triples = [('pcdm', 'hasFile', patent.file.uri)]
+        if patent.sparql_update(sparql_uri, auth, triples):
+            print("success!")
+        else:
+            print("failed!")
+            transaction.rollback()
+            continue'''
         
-        # update binary metadata graph
+        # update and patch binary metadata graph
         print("Updating binary metadata graph... ", end='')
-        print()
-        print()
-        p.file.print_graph()
-        
-        # update patent resource
-        print("Patching patent resource... ", end='')
-        print()
-        
-        # update binary resource
-        print("Patching file resource metadata... ", end='')
-        print()
+        sparql_uri = "{0}{1}{2}{3}".format(REST_ENDPOINT,
+                                           transaction.id,
+                                           patent.file.uri[len(REST_ENDPOINT):],
+                                           "/fcr:metadata"
+                                           )
+        triples = [('pcdm', 'fileOf', patent.uri)]
+        if patent.file.sparql_update(sparql_uri, auth, triples):
+            print("success!")
+        else:
+            print("failed!")
+            transaction.rollback()
+            continue
         
         # commit transaction or rollback
         print("Committing transaction... ", end='')
-        print()
+        if transaction.commit():
+            print("success!")
+        else:
+            print("failed!")
+            transaction.rollback()
         
+        print("\t".join([patent.uri, patent.file.uri]))
         # write to log
-        print("")
-        
-    
-        
+        logfile.write("\t".join([patent.uri, patent.file.uri]) + "\n")
+
+
+    # after all files processed, close log
     logfile.close()
-    
-    
+
+
 if __name__ == "__main__":
     main()
-    
-    
-'''
-        patent_uri = create_rdfsource(transaction['Location'])
-        file_uri = upload_file( transaction['Location'], 
-                                r.filepath, 
-                                r.checksum,
-                                patent_uri
-                                )
-        sparql_update(patent_uri, r.sparql_payload())
-        
 
-        response = requests.post(commit_uri, 
-                                 auth=(FEDORA_USER, FEDORA_PASSWORD)
-                                 )
-        if response.status_code == 204:
-            print('{0} transaction complete!'.format(response))
-            logfile.writeline("\t".join([r.title, patent_uri, file_uri]))
-        else:
-            print('Failed!')
-'''
+
